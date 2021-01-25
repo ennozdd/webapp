08c38e3b42b40746e2f7cc71eca6c6f56102cdd0
yemref07 <75582945+yemref07@users.noreply.github.com>
2021-01-25 15:53:40 +0300

    topbar

---
+++ templates/topbar.php
@@ -0,0 +1,9 @@
+	<!-- Top Bar -->
+	<div class="top-bar">
+		<div class="container">
+			<div class="col-12 text-right">
+				<p><a href="mail:ahsdja@gmail.com">Call Us</a></p>
+			</div>
+		</div>
+	</div>
+	<!-- End Top Bar -->
