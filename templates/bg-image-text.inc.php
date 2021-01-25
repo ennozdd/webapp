123aff8db41cb0d737fbb323d966ad2adf6967d9
yemref07 <75582945+yemref07@users.noreply.github.com>
2021-01-26 01:21:07 +0300

    replace the 3imagetext

---
+++ templates/bg-image-text.inc.php
@@ -0,0 +1,25 @@
+<div class="background1 py-5 text-white">
+		<div class="container">
+				<div class="row my-5 ">			
+					<div class="col-md-12">
+						<h4 class="my-4">Speed</h4>	
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit, quibusdam quasi minus numquam tempore reprehenderit eveniet incidunt quisquam quod officia culpa? Nesciunt repellat blanditiis quae adipisci, dolore ex alias sunt!</p>
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, veritatis? Totam, quia molestiae voluptas nam possimus excepturi nisi deserunt dolore placeat aliquam iste ea quaerat consectetur fugit quo harum saepe!</p>									
+					</div>
+				</div>
+						
+				<div class="row my-5">	
+					<div class="col-md-12">
+						<h4 class="my-4">Privacy</h4>	
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, veritatis? Totam, quia molestiae voluptas nam possimus excepturi nisi deserunt dolore placeat aliquam iste ea quaerat consectetur fugit quo harum saepe!</p>							
+					</div>
+				</div>
+						
+				<div class="row my-5">
+					<div class="col-md-12">					
+						<h4 class="my-4">Price</h4>	
+						<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, veritatis? Totam, quia molestiae voluptas nam possimus excepturi nisi deserunt dolore placeat aliquam iste ea quaerat consectetur fugit quo harum saepe!</p>							
+					</div>
+				</div>
+		</div>
+	</div>
