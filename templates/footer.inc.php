b9cd569fa3a3694a1d4f4fb4e57c40529dd12b93
yemref07 <75582945+yemref07@users.noreply.github.com>
2021-01-25 23:01:22 +0300

    footerinc

---
+++ templates/footer.inc.php
@@ -0,0 +1,40 @@
+	<!-- End Footer -->
+    <footer>
+			<div class="container">
+				<div class="row text-light text-center py-2 justify-content-center">
+					<div class="col-sm-10 col-md-8 col-lg-6">
+						<img src="media/img/enes-logo.png" alt="logo">
+						<p>Feel Freedom</p>
+						<ul class="social pt-3">
+							<li><a href="https://twitter.com/ennozdd?lang=en" target="_blank"><i  class="fab fa-twitter"></i></a></li>
+							<li><a href="" target="_blank"><i class="fab fa-facebook"></i></a></li>
+							<li><a href="" target="_blank"><i class="fab fa-instagram"></i></a></li>
+							<li><a href="" target="_blank"><i class="fab fa-youtube"></i></a></li>
+						</ul>
+					</div>
+
+				</div>
+			</div>
+		</footer>
+
+	<!-- Start Socket -->
+
+	<!-- End Socket -->
+		<div class="socket text-light text-center py-3">
+			<p>&copy;<a href="http://li1646-18.members.linode.com" target="_blank"</a>li1646-18.members.linode.com</a></p>
+		</div>
+
+	<!-- Script Source Files -->
+	<script src="https://kit.fontawesome.com/c083fb58ba.js" crossorigin="anonymous"></script>
+	<!-- jQuery -->
+	<script src="media/js/jquery-3.5.1.min.js"></script>
+	<!-- Bootstrap 4.5 JS -->
+	<script src="media/js/bootstrap.min.js"></script>
+	<!-- Popper JS -->
+	<script src="media/js/popper.min.js"></script>
+	<!-- Font Awesome -->
+	<script src="media/js/all.min.js"></script>
+
+	<!-- End Script Source Files -->
+</body>
+</html>
